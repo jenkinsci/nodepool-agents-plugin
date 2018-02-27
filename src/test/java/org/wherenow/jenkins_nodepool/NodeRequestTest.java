223fae1880ac9abe051e7bb220f61019dbc9a410
Hugh Saunders <hugh@wherenow.org>
2018-02-27 18:09:24 +0000

    still failing on tiget_types conflict

--- src/test/java/org/wherenow/jenkins_nodepool/NodeRequestTest.java
+++ src/test/java/org/wherenow/jenkins_nodepool/NodeRequestTest.java
@@ -24,9 +24,12 @@
 package org.wherenow.jenkins_nodepool;
 
 import com.google.gson.Gson;
+import java.net.URL;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.jar.Attributes;
+import java.util.jar.Manifest;
 import java.util.logging.Logger;
 import org.apache.curator.framework.CuratorFramework;
 import org.junit.Test;
@@ -34,58 +37,83 @@ import static org.junit.Assert.*;
 import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.ClassRule;
+import org.jvnet.tiger_types.Types;
 
 /**
  *
  * @author hughsaunders
  */
 public class NodeRequestTest {
+
     private static final Logger LOG = Logger.getLogger(NodeRequestTest.class.getName());
     static Gson gson;
     private String label = "testlabel";
     private CuratorFramework conn;
     private ZooKeeperClient zkc;
-    
+
     @ClassRule
     public static NodePoolRule npr = new NodePoolRule();
-    
+
     @BeforeClass
     public static void setUpClass() {
         gson = new Gson();
-      
-        
+
     }
-    
+
     @Before
-    public void setUp() throws Exception{
+    public void setUp() throws Exception {
         conn = npr.getCuratorConnection();
+
+        
+        //Temp code for debugging the tiger-types conflict
+        
+        // Example using HTMLEmail from Apache Commons Email 
+        Class theClass = Types.class;
+
+        // Find the path of the compiled class 
+        String classPath = theClass.getResource(theClass.getSimpleName() + ".class").toString();
+        LOG.severe("Class: " + classPath);
+
+        // Find the path of the lib which includes the class 
+        String libPath = classPath.substring(0, classPath.lastIndexOf("!"));
+        LOG.severe("Lib:   " + libPath);
+
+        // Find the path of the file inside the lib jar 
+        String filePath = libPath + "!/META-INF/MANIFEST.MF";
+        LOG.severe("File:  " + filePath);
+
+        // We look at the manifest file, getting two attributes out of it 
+        Manifest manifest = new Manifest(new URL(filePath).openStream());
+        Attributes attr = manifest.getMainAttributes();
+        LOG.severe("Manifest-Version: " + attr.getValue("Manifest-Version"));
+        LOG.severe("Implementation-Version: " + attr.getValue("Implementation-Version"));
     }
-    
+
     @Test
-    public void TestSerialisation(){
-        NodeRequest nr = new NodeRequest(conn, label);	
+    public void TestSerialisation() {
+        NodeRequest nr = new NodeRequest(conn, label);
         String json = nr.toString();
-        
-        LOG.fine("TestSerialisation json string: "+json);
-        
+
+        LOG.fine("TestSerialisation json string: " + json);
+
         // ensure the json is valid by deserialising it
         Map data = gson.fromJson(json, HashMap.class);
-        
+
         // Check a couple of key value pairs are as expected
-        assertEquals((String)data.get("state"), "requested");
-        assertEquals(((List)data.get("node_types")).get(0), label);
+        assertEquals((String) data.get("state"), "requested");
+        assertEquals(((List) data.get("node_types")).get(0), label);
     }
-    
+
     @Test
-    public void TestDeserialisation(){
+    public void TestDeserialisation() {
         String[] keys = {"node_types", "requestor", "state", "state_time"};
-        NodeRequest nr = new NodeRequest(conn, label);	
+        NodeRequest nr = new NodeRequest(conn, label);
         String json = nr.toString();
         NodeRequest nr2 = NodeRequest.fromJson(conn, json);
-        LOG.info("nr: "+nr);
-        LOG.info("nr2: "+nr2);
-        for (String key : keys){
-            LOG.info("key compare: "+key);
+        LOG.info("nr: " + nr);
+        LOG.info("nr2: " + nr2);
+        for (String key : keys) {
+            LOG.info("key compare: " + key);
             assertEquals(nr.get(key), nr2.get(key));
         }
         assertEquals(nr, nr2);
