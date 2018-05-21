04d898ddd3701add2ddbacd357c8dfe4e27ccec5
David Deal <david.deal@rackspace.com>
2018-05-20 19:51:00 -0700

    RE-1496 NodePool View Updates
    - Updated table view to add sorting, filtering and pagination
    - Added CSS, Images, and JS support files to styling and table presentation logic
    - Removed complete column, renamed result to Provision Status, updated status field value
    - Added custom column renderer for NodePool Cluster column
    - Cleaned up NodePool Job, History and Attempts support classes - added routines to support the presentation layer.
    - Added image/layer optimizations and labels to dev/test Dockerfile
    - Added debug for dev/test NodePool launcher launch bash script

--- docker/launcher.sh
+++ docker/launcher.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
 if [ -z "$NODEPOOL_CLOUD_NAME" ]; then
     echo "Need NODEPOOL_CLOUD_NAME to be set name of cloud in clouds.yaml"
@@ -15,6 +15,9 @@ if [ -z "$NODEPOOL_KEYPAIR_NAME" ]; then
     exit 1
 fi
 
+set -eu
+set -o pipefail
+
 echo "Setting cloud to ${NODEPOOL_CLOUD_NAME}"
 sed -i s/%NODEPOOL_CLOUD_NAME%/${NODEPOOL_CLOUD_NAME}/g /etc/nodepool/nodepool.yaml
 
@@ -23,5 +26,12 @@ sed -i s/%NODEPOOL_REGION_NAME%/${NODEPOOL_REGION_NAME}/g /etc/nodepool/nodepool
 
 echo "Setting keypair to ${NODEPOOL_KEYPAIR_NAME}"
 sed -i s/%NODEPOOL_KEYPAIR_NAME%/${NODEPOOL_KEYPAIR_NAME}/g /etc/nodepool/nodepool.yaml
+
+# DEBUG
+echo "Running with logging configuration:"
+cat /etc/nodepool/logging.conf
+echo "Running with application configuration:"
+cat /etc/nodepool/nodepool.yaml
+# END DEBUG
 
 nodepool-launcher -d -l /etc/nodepool/logging.conf
---
+++ src/main/webapp/javascript/init.js
@@ -0,0 +1,23 @@
+/*
+ * Initialize the data table.  Specifies a few options:
+ * - sets the table length (number of rows) options to custom values
+ * - sets a default order
+ * - Save the state of a table (its paging position, ordering state etc) so that is can be restored when the user
+ *   reloads a page, or comes back to the page after visiting a sub-page.
+ *
+ * More fun options here: https://datatables.net/examples/basic_init/
+ */
+$(document).ready(function () {
+    $('#nodepool-table').DataTable({
+        "lengthMenu": [[25, 50, 75, -1], [25, 50, 75, "All"]],
+        "order": [[0, "asc"]],
+        stateSave: true,
+        // Need a custom renderer for NodePool Cluster column
+        "columnDefs": [{
+            "targets": 4,
+            "render": function (data, type, row, meta) {
+                return data.replace(new RegExp(',', 'g'), ' ');
+            }
+        }]
+    });
+});
